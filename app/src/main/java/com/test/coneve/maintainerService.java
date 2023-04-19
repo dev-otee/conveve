1fde0f61fcd96faa77fd725b623b958022fbec13
dev-otee <nikhil22045@iiitd.ac.in>
2023-04-19 12:00:28 +0530

    service added

--- app/src/main/java/com/test/coneve/maintainerService.java
+++ app/src/main/java/com/test/coneve/maintainerService.java
@@ -6,18 +6,20 @@ import android.os.IBinder;
 
 import androidx.lifecycle.MutableLiveData;
 
-import com.google.firebase.auth.FirebaseAuth;
+
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class maintainerService extends Service {
 
-
+    private int bindings;
     public MutableLiveData<FirebaseUser> profile;
     //TODO :  Add event data
 
     public maintainerService() {
-
+        bindings = 0;
+        FirebaseDatabase.getInstance().getReference("Events");
     }
     @Override
     public int onStartCommand(Intent intent,int flags,int startId)
@@ -28,6 +30,15 @@ public class maintainerService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
+        bindings++;
 
+        return new servicedataInterface(this);
+    }
+
+    @Override
+    public boolean onUnbind (Intent intent)
+    {
+        bindings--;
+        return false;
     }
 }
